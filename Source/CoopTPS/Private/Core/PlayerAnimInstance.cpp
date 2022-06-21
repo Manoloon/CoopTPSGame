758fcad642494f08515e3a3882eb98cda08cef95
Pablo Sanchez <14944505+Manoloon@users.noreply.github.com>
2022-06-21 14:26:53 -0300

    animationInstance para el pawn

---
+++ Source/CoopTPS/Private/Core/PlayerAnimInstance.cpp
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Core/PlayerAnimInstance.h"
+
+#include "SCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "KismetAnimationLibrary.h"
+#include "Kismet/KismetMathLibrary.h"
+
+void UPlayerAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+	if(PawnCharacter == nullptr)
+	{
+		PawnCharacter = Cast<ASCharacter>(TryGetPawnOwner());
+	}
+	if(PawnCharacter ==nullptr){return;}
+	bIsCrouched = PawnCharacter->bIsCrouched;
+	bIsInAir = PawnCharacter->GetCharacterMovement()->IsFalling();
+	FVector Velocity = PawnCharacter->GetVelocity();
+	Velocity.Z = 0.f;
+	Speed =  Velocity.Size();
+	bIsAccelerating = (PawnCharacter->GetCharacterMovement()->GetCurrentAcceleration().Size()> 0.0f);
+	bWeaponEquipped = PawnCharacter->IsWeaponEquipped();
+	bIsAiming = PawnCharacter->IsAiming();
+	Direction = UKismetAnimationLibrary::CalculateDirection(Velocity,PawnCharacter->GetActorRotation());
+	//strafing
+	const FRotator AimRotation = PawnCharacter->GetBaseAimRotation();
+	const FRotator MovementRotation = UKismetMathLibrary::MakeRotFromX(PawnCharacter->GetVelocity());
+	const FRotator LocalDeltaRot = UKismetMathLibrary::NormalizedDeltaRotator(MovementRotation,
+	AimRotation);
+	DeltaRotation=FMath::RInterpTo(DeltaRotation,LocalDeltaRot,DeltaSeconds,7.f);
+	YawOffset = DeltaRotation.Yaw;
+	// Lean
+	CharacterRotationLastFrame = CharacterRotation;
+	CharacterRotation = PawnCharacter->GetActorRotation();
+	const FRotator DeltaActorRotation = UKismetMathLibrary::NormalizedDeltaRotator(CharacterRotation,
+	CharacterRotationLastFrame);
+	const float TargetRotation = DeltaActorRotation.Yaw /DeltaSeconds;
+	const float InterpLean = FMath::FInterpTo(Lean,TargetRotation,DeltaSeconds,6.f);
+	Lean = FMath::Clamp(InterpLean,-90.f,90.f);
+}
---
+++ Source/CoopTPS/Private/Core/PlayerAnimInstance.h
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "PlayerAnimInstance.generated.h"
+
+class ASCharacter;
+UCLASS(Abstract)
+class UPlayerAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	UPROPERTY()
+	ASCharacter* PawnCharacter =nullptr;
+public:
+	UPROPERTY(BlueprintReadOnly,EditDefaultsOnly,Category ="Settings")
+	bool bIsCrouched=false;
+	UPROPERTY(BlueprintReadOnly,EditDefaultsOnly,Category ="Settings")
+	float Speed =0.f;
+	UPROPERTY(BlueprintReadOnly,EditDefaultsOnly,Category ="Settings")
+	float YawOffset=0.f;
+	UPROPERTY(BlueprintReadOnly,EditDefaultsOnly,Category ="Settings")
+	float Lean=0.0f;
+	UPROPERTY(BlueprintReadOnly,EditDefaultsOnly,Category ="Settings")
+	float Direction =0.0f;
+	UPROPERTY(BlueprintReadOnly,EditDefaultsOnly,Category ="Settings")
+	bool bIsInAir=false;
+	UPROPERTY(BlueprintReadOnly,EditDefaultsOnly,Category ="Settings")
+	bool bWeaponEquipped = false;
+	UPROPERTY(BlueprintReadOnly,EditDefaultsOnly,Category ="Settings")
+	bool bIsAccelerating=false;
+	UPROPERTY(BlueprintReadOnly,EditDefaultsOnly,Category ="Settings")
+	bool bIsAiming=false;
+	UPROPERTY(BlueprintReadOnly,EditDefaultsOnly,Category ="Settings")
+	bool bDie =false;
+
+	FRotator DeltaRotation;
+	FRotator CharacterRotation;
+	FRotator CharacterRotationLastFrame;
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+};
